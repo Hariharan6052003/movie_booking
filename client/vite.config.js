1effe87f709512896e0cb901ce15ec7165a92177
Hariharan6052003 <145706571+Hariharan6052003@users.noreply.github.com>
2025-03-17 01:46:53 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/theatres': {
-        target: 'https://movie-booking-mh54.onrender.com', // Your backend server URL
+        target: 'http://localhost:5000', // Your backend server URL
         changeOrigin: true,
       },
     },
